f8c6e5c570792deca22cc5228568294b1b163099
Sam Bokai <sam.bokai@gmail.com>
2025-10-18 20:12:32 +0200

    feat: add targetTile parameter for MIRV unit to enable detection of inbound MIRVs

--- src/core/game/Game.ts
+++ src/core/game/Game.ts
@@ -267,7 +267,9 @@ export interface UnitParamsMap {
 
   [UnitType.City]: Record<string, never>;
 
-  [UnitType.MIRV]: Record<string, never>;
+  [UnitType.MIRV]: {
+    targetTile?: number;
+  };
 
   [UnitType.MIRVWarhead]: {
     targetTile?: number;
